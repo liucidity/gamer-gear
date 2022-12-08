03ec4f5263d7c0d0f75abcdba4f196f739210175
Stanley Surjanto <stan.surjanto@gmail.com>
2022-12-08 00:53:58 -0700

    style: change w-screen to w-full

--- pages/home/index.tsx
+++ pages/home/index.tsx
@@ -28,7 +28,7 @@ const topProducts = [
 
 const Home = ({ children }: Props) => {
   return (
-    <div className="flex h-screen w-screen flex-col items-center justify-between pt-24">
+    <div className="flex h-screen w-full flex-col items-center justify-between pt-24">
       <section className="flex w-full flex-col items-center">
         <p className="px-5 prose-sm md:prose-xl">
           Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
