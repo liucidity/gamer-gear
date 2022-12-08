757e899b4b0e69349ad7725aa3f368508f249b1d
Stanley Surjanto <stan.surjanto@gmail.com>
2022-12-07 22:55:07 -0700

    docs: add type annotation for props

--- components/Widgets/MenuButtons.tsx
+++ components/Widgets/MenuButtons.tsx
@@ -1,9 +1,9 @@
 import React from "react";
 
-const MenuButtons = ({ children, link }) => {
+const MenuButtons = (props: { children:any , link:string }) => {
   return (
-    <a className="mt-2 ml-5 border-2 border-black md:mt-0" href={link}>
-      {children}
+    <a className="mt-2 ml-5 border-2 border-black md:mt-0" href={props.link}>
+      {props.children}
     </a>
   );
 };
