fa83f8a1b1cd0e845211e4d75708cb4926820a94
Stanley Surjanto <stan.surjanto@gmail.com>
2022-12-08 16:43:43 -0700

    style: font styles for logo

--- components/Widgets/Logo.tsx
+++ components/Widgets/Logo.tsx
@@ -3,8 +3,8 @@ import React from "react";
 const Logo = () => {
   return (
     <header className="flex justify-center items-center ">
-      <h1>GAMER</h1>
-      <h1>GEAR</h1>
+      <h1 className="font-sans font-bold text-off-white text-3xl tracking-wide subpixel-antialiased">GAMER</h1>
+      <h1 className="font-sans font-bold text-black text-3xl tracking-wide subpixel-antialiased">GEAR</h1>
     </header>
   );
 };
