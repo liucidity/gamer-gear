b7091d140f0910d847e2fa59c31119d75e3b0501
Stanley Surjanto <stan.surjanto@gmail.com>
2022-12-07 19:10:14 -0700

    feat: add dropdown handler and state

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,24 +1,36 @@
-import React from 'react'
+import React, { useState } from 'react'
 import MenuButtons from './Widgets/MenuButtons'
 
 const Navbar = () => {
+  const [dropDownOpen, setDropDownOpen] = useState(false);
+
+  const handleDropDown = () => {
+    dropDownOpen ? setDropDownOpen(false) : setDropDownOpen(true)
+  }
   return (
-    <nav className='border-2 border-blue-700 w-screen px-14 py-5'>
-      <div className='flex justify-between'>
-        <div className='border-2 border-black'>
-          Logo Here
+    <>
+      <nav className='border-2 border-blue-700 fixed top-0 w-screen px-14 py-5'>
+        <div className='flex justify-between'>
+          <div className='border-2 border-black'>
+            Logo Here
+          </div>
+          <button className='border-2 border-purple-500 flex md:hidden' onClick={() => handleDropDown()}>DropDown</button>
+          <div className='hidden md:flex border-2 border-black'>
+            <MenuButtons link={'/products'}>Products</MenuButtons>
+            <MenuButtons link={'/about'}>About</MenuButtons>
+            <MenuButtons link={'/players'}>Players</MenuButtons>
+          </div>
         </div>
-        <div className='border-2 border-black flex'>
+      </nav>
+      { dropDownOpen &&
+        <div className='flex flex-col items-center justify-start border-2 border-black'>
           <MenuButtons link={'/products'}>Products</MenuButtons>
           <MenuButtons link={'/about'}>About</MenuButtons>
           <MenuButtons link={'/players'}>Players</MenuButtons>
         </div>
-      </div>
-    </nav>
+      }
+    </>
   )
 }
 
 export default Navbar
-
-// add later
-// fixed top-0
