d026fd93f2cea01522b44df2b9801a531346a899
Stanley Surjanto <stan.surjanto@gmail.com>
2022-12-08 00:45:39 -0700

    style: add lorem and carousel header

--- pages/home/index.tsx
+++ pages/home/index.tsx
@@ -4,9 +4,8 @@ import SnapCarousel from "../../components/SnapCarousel";
 // ---- Type Definitions
 // ** If the Home component below accepts more props, add them and their respective types in this Type declaration, and then destructure below **
 type Props = {
-  children: any,
-}
-
+  children: any;
+};
 
 const topProducts = [
   {
@@ -29,16 +28,22 @@ const topProducts = [
 
 const Home = ({ children }: Props) => {
   return (
-    <div className="flex h-screen w-screen flex-col items-center justify-between border-2 border-slate-500 px-10 pt-24">
-      <section className="flex w-screen flex-col items-center border-2 border-green-500">
-        <p className="flex w-screen items-center justify-center border-2 border-pink-500">
-          Hero
+    <div className="flex h-screen w-screen flex-col items-center justify-between pt-24">
+      <section className="flex w-full flex-col items-center">
+        <p className="prose-sm md:prose-xl">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
         </p>
-        <div className="flex w-screen items-center justify-center border-2 border-pink-500">
+        <div className="flex w-full items-center justify-center">
           Searchbar
         </div>
       </section>
-      <SnapCarousel products={topProducts} />
+      <section className="flex flex-col">
+        <header className="flex items-center justify-between px-5">
+          <div>Top Products</div>
+          <a>View More</a>
+        </header>
+        <SnapCarousel products={topProducts} />
+      </section>
     </div>
   );
 };
