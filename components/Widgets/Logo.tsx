73cbb63fca40a9b7e759b5998a1e736673664e6e
Stanley Surjanto <stan.surjanto@gmail.com>
2022-12-08 16:12:01 -0700

    init

---
+++ components/Widgets/Logo.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Logo = () => {
+  return (
+    <header className="flex justify-center items-center ">
+      <h1>GAMER</h1>
+      <h1>GEAR</h1>
+    </header>
+  );
+};
+
+export default Logo;
