723810a9c07912e14bfe8c69c82b56e032c211d5
Stanley Surjanto <stan.surjanto@gmail.com>
2022-12-08 16:59:43 -0700

    style: text color and on hover change

--- components/Widgets/MenuButtons.tsx
+++ components/Widgets/MenuButtons.tsx
@@ -2,8 +2,8 @@ import React from "react";
 
 const MenuButtons = (props: { children:any , link:string }) => {
   return (
-    <a className="mt-2 ml-5 border-2 border-black md:mt-0" href={props.link}>
-      {props.children}
+    <a className="mt-2 ml-6 md:mt-0" href={props.link}>
+      <h1 className="text-white hover:text-black font-sans font-normal text-xl">{props.children}</h1>
     </a>
   );
 };
