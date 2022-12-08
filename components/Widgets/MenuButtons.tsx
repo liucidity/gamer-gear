e020fd1e159616a5aa16fb6f01a3ad502a04057c
Stanley Surjanto <stan.surjanto@gmail.com>
2022-12-07 19:29:15 -0700

    style: add margin top on mobile view

--- components/Widgets/MenuButtons.tsx
+++ components/Widgets/MenuButtons.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const MenuButtons = ({children, link}) => {
   return (
-    <a className="border-2 border-black ml-5" href={link}>{children}</a>
+    <a className="mt-2 border-2 border-black md:mt-0 ml-5" href={link}>{children}</a>
   )
 }
 
