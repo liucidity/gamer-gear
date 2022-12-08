86be7ff922554af8bfd6ed51983841febe574792
Stanley Surjanto <stan.surjanto@gmail.com>
2022-12-07 19:58:06 -0700

    feat: reusable navbar redirect buttons

--- components/Widgets/MenuButtons.tsx
+++ components/Widgets/MenuButtons.tsx
@@ -1,9 +1,11 @@
-import React from 'react'
+import React from "react";
 
-const MenuButtons = ({children, link}) => {
+const MenuButtons = ({ children, link }) => {
   return (
-    <a className="mt-2 border-2 border-black md:mt-0 ml-5" href={link}>{children}</a>
-  )
-}
+    <a className="mt-2 ml-5 border-2 border-black md:mt-0" href={link}>
+      {children}
+    </a>
+  );
+};
 
-export default MenuButtons
+export default MenuButtons;
