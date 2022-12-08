464367b75e574fd4976190dd37cf6158c1fff181
Stanley Surjanto <stan.surjanto@gmail.com>
2022-12-07 19:56:58 -0700

    style: prettier format tailwind classes

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,36 +1,39 @@
-import React, { useState } from 'react'
-import MenuButtons from './Widgets/MenuButtons'
+import React, { useState } from "react";
+import MenuButtons from "./Widgets/MenuButtons";
 
 const Navbar = () => {
   const [dropDownOpen, setDropDownOpen] = useState(false);
 
   const handleDropDown = () => {
-    dropDownOpen ? setDropDownOpen(false) : setDropDownOpen(true)
-  }
+    dropDownOpen ? setDropDownOpen(false) : setDropDownOpen(true);
+  };
   return (
     <>
-      <nav className='border-2 border-blue-700 fixed top-0 w-screen px-14 py-5'>
-        <div className='flex justify-between'>
-          <div className='border-2 border-black'>
-            Logo Here
-          </div>
-          <button className='border-2 border-purple-500 flex md:hidden' onClick={() => handleDropDown()}>DropDown</button>
-          <div className='hidden md:flex border-2 border-black'>
-            <MenuButtons link={'/products'}>Products</MenuButtons>
-            <MenuButtons link={'/about'}>About</MenuButtons>
-            <MenuButtons link={'/players'}>Players</MenuButtons>
+      <nav className="fixed top-0 z-10 w-screen border-2 border-blue-700 px-14 py-5">
+        <div className="flex justify-between">
+          <div className="border-2 border-black">Logo Here</div>
+          <button
+            className="flex border-2 border-purple-500 md:hidden"
+            onClick={() => handleDropDown()}
+          >
+            DropDown
+          </button>
+          <div className="hidden border-2 border-black md:flex">
+            <MenuButtons link={"/products"}>Products</MenuButtons>
+            <MenuButtons link={"/about"}>About</MenuButtons>
+            <MenuButtons link={"/players"}>Players</MenuButtons>
           </div>
         </div>
-        { dropDownOpen &&
-          <div className='flex flex-col items-center justify-start py-4    border-2 border-black'>
-            <MenuButtons link={'/products'}>Products</MenuButtons>
-            <MenuButtons link={'/about'}>About</MenuButtons>
-            <MenuButtons link={'/players'}>Players</MenuButtons>
+        {dropDownOpen && (
+          <div className="flex flex-col items-center justify-start border-2    border-black py-4">
+            <MenuButtons link={"/products"}>Products</MenuButtons>
+            <MenuButtons link={"/about"}>About</MenuButtons>
+            <MenuButtons link={"/players"}>Players</MenuButtons>
           </div>
-        }
+        )}
       </nav>
     </>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
