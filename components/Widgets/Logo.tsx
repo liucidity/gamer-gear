a66f14753611452a174607807f5c69d0f2051c0f
Stanley Surjanto <stan.surjanto@gmail.com>
2022-12-08 16:53:44 -0700

    feat: logo leads to /home on click

--- components/Widgets/Logo.tsx
+++ components/Widgets/Logo.tsx
@@ -2,10 +2,10 @@ import React from "react";
 
 const Logo = () => {
   return (
-    <header className="flex justify-center items-center ">
+    <a className="flex justify-center items-center" href="/home">
       <h1 className="font-sans font-bold text-off-white text-3xl tracking-wide subpixel-antialiased">GAMER</h1>
       <h1 className="font-sans font-bold text-black text-3xl tracking-wide subpixel-antialiased">GEAR</h1>
-    </header>
+    </a>
   );
 };
 
