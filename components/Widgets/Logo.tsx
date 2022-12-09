ad93f9bdb0b4e22beefc540b69b6a14e3afa0b59
Stanley Surjanto <stan.surjanto@gmail.com>
2022-12-08 17:05:01 -0700

    style: add margin right

--- components/Widgets/Logo.tsx
+++ components/Widgets/Logo.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const Logo = () => {
   return (
-    <a className="flex justify-center items-center" href="/home">
+    <a className="flex justify-start items-end mr-10" href="/home">
       <h1 className="font-sans font-bold text-off-white text-3xl tracking-wide subpixel-antialiased">GAMER</h1>
       <h1 className="font-sans font-bold text-black text-3xl tracking-wide subpixel-antialiased">GEAR</h1>
     </a>
