ac288334fa6e9f9ed664534e2db3f02f0cd0f822
Stanley Surjanto <stan.surjanto@gmail.com>
2022-12-09 21:25:03 -0700

    style: remove text-black for default text-black-main

--- pages/home/index.tsx
+++ pages/home/index.tsx
@@ -30,14 +30,14 @@ const topProducts = [
 const Home = ({ children }: Props) => {
   return (
     <div className="flex h-full w-full flex-col items-center justify-between pt-24">
-      <header className="flex h-52 w-[90%] flex-col items-center border-2 border-purple-main">
+      <header className="flex h-52 w-[90%] md:w-5/6 flex-col items-center border-2 border-purple-main">
         <h1 className="prose-sm px-5 md:prose-xl">GAMERGEAR</h1>
         <h2>Test Tagline For Hero Section</h2>
       </header>
       <Searchbar />
-      <section className="flex w-[90%] flex-col border-2 border-purple-main">
-        <header className="flex items-center justify-between px-5">
-          <div>Top Products</div>
+      <section className="flex w-[90%] md:w-5/6 flex-col mt-5 border-2 border-purple-main">
+        <header className="flex items-center justify-between border-2 border-blue-500">
+          <h1 className="border-2 border-black font-sans font-bold text-xl md:text-3xl">Top Products</h1>
           <a>View More</a>
         </header>
         <SnapCarousel products={topProducts} />
