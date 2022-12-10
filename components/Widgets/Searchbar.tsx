505e879c646ff27ca221f010b1bca235a9b1504c
Stanley Surjanto <stan.surjanto@gmail.com>
2022-12-09 20:03:09 -0700

    fix: fix typos in path tag

--- components/Widgets/Searchbar.tsx
+++ components/Widgets/Searchbar.tsx
@@ -11,7 +11,7 @@ const Searchbar = () => {
           placeholder="Search Products, Players & Teams..."
         />
         <svg className="absolute ml-3 h-6 w-6 text-gray-400 pointer-events-none"  fill="none" viewBox="0 0 24 24" stroke="currentColor">
-          <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"/>
+          <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"/>
         </svg>
       </div>
     </form>
