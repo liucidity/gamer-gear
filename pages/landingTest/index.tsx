8270bc9a6e43de0f177698a2cd541ac067b028dc
Stanley Surjanto <stan.surjanto@gmail.com>
2022-12-13 19:24:47 -0700

    landingpage2 created

---
+++ pages/landingTest/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import HeroPanels from '../../components/HeroPanels';
+
+const LandingPage = () => {
+  return (
+    <section className='h-full w-full flex flex-col items-center'>
+      <HeroPanels />
+    </section>
+  )
+}
+
+export default LandingPage;
