9f7a76ee821acbc094a01bfad63aabcbb4f45b9e
Stanley Surjanto <stan.surjanto@gmail.com>
2022-12-13 20:47:41 -0700

    feat: useState for card slide animation

--- components/HeroPanels.tsx
+++ components/HeroPanels.tsx
@@ -4,12 +4,43 @@ import { motion, AnimatePresence } from "framer-motion";
 type Props = {};
 
 const HeroPanels = ({}: Props) => {
+  const [isHovered, setIsHovered] = useState(false);
+  const handleMouseEnter = () => {
+    setIsHovered(true);
+    console.log(isHovered);
+  };
+  const handleMouseLeave = () => {
+    setIsHovered(false);
+    console.log(isHovered);
+  };
+
+  const bannerFM = {
+    hover: {
+      x: "-100%",
+    },
+    initial: {
+      x: "0",
+    },
+  };
+
   return (
     <AnimatePresence>
       <header className="h-1/2 w-[90%] border-2 border-black md:w-2/3">
-        <motion.div className="group flex h-full w-full md:w-1/3">
-          <motion.div className="flex h-full w-full bg-red-500 group-hover:hidden"></motion.div>
-          <motion.div className="hidden h-full w-full bg-blue-500 group-hover:flex">
+        <motion.div
+          className="flex h-full w-full overflow-hidden border-2 border-purple-main md:w-1/3"
+          onMouseEnter={handleMouseEnter}
+          onMouseLeave={handleMouseLeave}
+        >
+          <motion.div
+            className="flex h-full w-full flex-shrink-0 bg-red-500"
+            variants={bannerFM}
+            animate={isHovered ? "hover" : "initial"}
+          ></motion.div>
+          <motion.div
+            className="flex h-full w-full flex-shrink-0 bg-blue-500"
+            variants={bannerFM}
+            animate={isHovered ? "hover" : "initial"}
+          >
             <button onClick={() => console.log("click")}>click here</button>
           </motion.div>
         </motion.div>
