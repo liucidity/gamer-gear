8343e33a2b29a18f8ad384752ae03761bf9d2ad6
Stanley Surjanto <stan.surjanto@gmail.com>
2022-12-14 19:58:31 -0700

    docs: add ids for clarity

--- components/HeroPanels.tsx
+++ components/HeroPanels.tsx
@@ -4,16 +4,6 @@ import { motion, AnimatePresence, useAnimation } from "framer-motion";
 type Props = {};
 
 const HeroPanels = ({}: Props) => {
-  const [isHovered, setIsHovered] = useState(false);
-  const handleMouseEnter = () => {
-    setIsHovered(true);
-    console.log(isHovered);
-  };
-  const handleMouseLeave = () => {
-    setIsHovered(false);
-    console.log(isHovered);
-  };
-
   const controls = useAnimation();
   const mouseEnterControls = () => {
     controls.start("hover");
@@ -25,7 +15,6 @@ const HeroPanels = ({}: Props) => {
   const bannerFM = {
     hover: {
       x: "-100%",
-      
     },
     initial: {
       x: "0",
@@ -34,44 +23,26 @@ const HeroPanels = ({}: Props) => {
 
   return (
     <AnimatePresence>
-      <header className="h-1/2 w-[90%] border-2 border-black md:w-2/3">
+      <motion.section
+        id="banner_items-container"
+        className="flex h-full w-full flex-shrink-0 overflow-x-hidden border-slate-600 last-of-type:border-r-0 md:w-1/3 md:border-r-2"
+        onMouseEnter={mouseEnterControls}
+        onMouseLeave={mouseLeaveControls}
+      >
         <motion.div
-          className="flex h-full w-full overflow-hidden border-2 border-purple-main md:w-1/3"
-          onMouseEnter={handleMouseEnter}
-          onMouseLeave={handleMouseLeave}
-        >
-          <motion.div
-            className="flex h-full w-full flex-shrink-0 bg-red-500"
-            variants={bannerFM}
-            animate={isHovered ? "hover" : "initial"}
-          ></motion.div>
-          <motion.div
-            className="flex h-full w-full flex-shrink-0 bg-blue-500"
-            variants={bannerFM}
-            animate={isHovered ? "hover" : "initial"}
-          >
-            <button onClick={() => console.log("click")}>click here</button>
-          </motion.div>
-        </motion.div>
+          id="banner_items"
+          className="flex h-full w-full flex-shrink-0 bg-black-main"
+          variants={bannerFM}
+          animate={controls}
+        ></motion.div>
         <motion.div
-          className="flex h-full w-full overflow-hidden border-2 border-purple-main md:w-1/3"
-          onMouseEnter={mouseEnterControls}
-          onMouseLeave={mouseLeaveControls}
+          className="hidden h-full w-full flex-shrink-0 bg-blue-500 md:flex"
+          variants={bannerFM}
+          animate={controls}
         >
-          <motion.div
-            className="flex h-full w-full flex-shrink-0 bg-red-500"
-            variants={bannerFM}
-            animate={controls}
-          ></motion.div>
-          <motion.div
-            className="flex h-full w-full flex-shrink-0 bg-blue-500"
-            variants={bannerFM}
-            animate={controls}
-          >
-            <button onClick={() => console.log("click")}>click here</button>
-          </motion.div>
+          <button onClick={() => console.log("click")}>click here</button>
         </motion.div>
-      </header>
+      </motion.section>
     </AnimatePresence>
   );
 };
