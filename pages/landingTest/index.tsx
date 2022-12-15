9b9ae5a9e09b51afd6df76faea1851ca81bb47e9
Stanley Surjanto <stan.surjanto@gmail.com>
2022-12-14 22:27:29 -0700

    docs: refactor banners into own component

--- pages/landingTest/index.tsx
+++ pages/landingTest/index.tsx
@@ -1,15 +1,30 @@
 import React from "react";
-import HeroPanels from "../../components/HeroPanels";
+import HeroSection from "../../components/HeroSection";
 
+// ---- Type Declarations ----
+type PlayerObjectList = { username: string; id: number }[];
+
+// ---- Data ----
+let playersData:PlayerObjectList = [
+  {
+    id: 1,
+    username: "TenZ",
+  },
+  {
+    id: 2,
+    username: "S1mple",
+  },
+  {
+    id: 3,
+    username: "NiKo",
+  },
+];
+
+
+// ---- Main Landing Page Component ----
 const LandingPage = () => {
   return (
-    <section className="flex h-full w-full flex-col items-center justify-center bg-black-main">
-      <header id="banner-container" className="flex h-4/5 w-full md:w-5/6">
-        <HeroPanels />
-        <HeroPanels />
-        <HeroPanels />
-      </header>
-    </section>
+    <HeroSection playersData={playersData}/>
   );
 };
 
