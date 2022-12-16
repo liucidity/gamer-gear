30defcce35fc53b90fa2a7c44c05c67522943d75
Stanley Surjanto <stan.surjanto@gmail.com>
2022-12-15 20:31:30 -0700

    style: text-hover to purple dark

--- src/components/Widgets/MenuButtons.tsx
+++ src/components/Widgets/MenuButtons.tsx
@@ -3,7 +3,7 @@ import React from "react";
 const MenuButtons = (props: { children:any , link:string }) => {
   return (
     <a className="mt-2 mr-10 md:mt-0 " href={props.link}>
-      <h1 className="text-white font-sans font-normal hover:text-purple-400 text-xl subpixel-antialiased">{props.children}</h1>
+      <h1 className="text-white font-sans font-normal hover:text-purple-dark text-xl subpixel-antialiased">{props.children}</h1>
     </a>
   );
 };
