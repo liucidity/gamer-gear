69818186fda578a8276302165e7195b5fd4d4bbc
Stanley Surjanto <stan.surjanto@gmail.com>
2022-12-20 01:53:10 -0700

    prettier

--- src/components/Widgets/BannerProductCard.tsx
+++ src/components/Widgets/BannerProductCard.tsx
@@ -29,25 +29,22 @@ const BannerProductCard = ({ product_name, key, category }: Props) => {
   return (
     <motion.div
       key={key}
-      className="relative flex w-4/5 flex-col items-center justify-evenly hover:scale-110 md:h-96 shadow-inner"
+      className="relative flex w-4/5 flex-col items-center justify-evenly shadow-inner hover:scale-110 md:h-96"
     >
       {/* Will run into trouble here with the round container sizing - hopefully a real image helps */}
       <div className="absolute left-0 z-10 ml-2 h-[6.5rem] w-[6.5rem] rounded-full bg-white shadow-xl shadow-purple-main/40 ring-4 ring-purple-main ring-offset-0">
-        <Image
-          src={productImageTest}
-          alt={"Product_image"}
-          width={100}
-          height={100}
-        />
+        <Image src={productImageTest} alt={"Product_image"} fill />
       </div>
-      <div className="flex h-1/2 w-10/12 flex-col items-center justify-evenly rounded-lg bg-white pl-20 drop-shadow-lg py-2">
-        <h1 className="text-center font-sans text-base font-semibold tracking-wide">
+      <div className="flex h-1/2 w-10/12 flex-col items-center justify-evenly rounded-lg bg-white py-2 pl-20 drop-shadow-lg">
+        <h1 className="px-2 text-center font-sans text-sm font-semibold tracking-wide  md:text-sm 2xl:text-lg">
           {product_category}
         </h1>
-        <h1 className="text-lg break-words text-center font-sans font-medium tracking-wide">
+        <h1 className="max-w-full text-clip break-words px-2 text-center font-sans text-sm font-medium tracking-wide md:text-sm 2xl:text-lg">
           {product_name}
         </h1>
-        <h1 className="text-center font-sans font-thin">Brand Name Here</h1>
+        <h1 className="px-2 text-center font-sans font-thin">
+          Brand Name Here
+        </h1>
       </div>
     </motion.div>
   );
