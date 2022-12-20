df3fa8d9b2c4890520458fca06066a31aa4e3c8a
Stanley Surjanto <stan.surjanto@gmail.com>
2022-12-20 01:27:48 -0700

    style: add product images and style the cards

--- src/components/Widgets/BannerProductCard.tsx
+++ src/components/Widgets/BannerProductCard.tsx
@@ -13,26 +13,26 @@ const BannerProductCard = ({ product_name, key, category }: Props) => {
   let product_category = "";
   switch (category) {
     case 0:
-      product_category = "Keyboard";
+      product_category = "Keyboard".toUpperCase();
       break;
     case 1:
-      product_category = "Mouse";
+      product_category = "Mouse".toUpperCase();
       break;
     case 2:
-      product_category = "Headset";
+      product_category = "Headset".toUpperCase();
       break;
     case 3:
-      product_category = "Monitor";
+      product_category = "Monitor".toUpperCase();
       break;
   }
 
   return (
     <motion.div
       key={key}
-      className="relative flex h-full w-full flex-col items-center justify-evenly"
+      className="relative flex w-4/5 flex-col items-center justify-evenly hover:scale-110 md:h-96 shadow-inner"
     >
       {/* Will run into trouble here with the round container sizing - hopefully a real image helps */}
-      <div className="absolute left-0 z-10 ml-3 h-[6.5rem] w-[6.5rem] rounded-full bg-purple-light">
+      <div className="absolute left-0 z-10 ml-2 h-[6.5rem] w-[6.5rem] rounded-full bg-white shadow-xl shadow-purple-main/40 ring-4 ring-purple-main ring-offset-0">
         <Image
           src={productImageTest}
           alt={"Product_image"}
@@ -40,9 +40,13 @@ const BannerProductCard = ({ product_name, key, category }: Props) => {
           height={100}
         />
       </div>
-      <div className="flex h-1/2 w-10/12 flex-col items-center justify-evenly rounded-lg bg-white pl-24 drop-shadow-lg">
-        <h1 className="text-center font-sans font-thin">{product_category}</h1>
-        <h1 className="text-center font-sans font-thin">{product_name}</h1>
+      <div className="flex h-1/2 w-10/12 flex-col items-center justify-evenly rounded-lg bg-white pl-20 drop-shadow-lg py-2">
+        <h1 className="text-center font-sans text-base font-semibold tracking-wide">
+          {product_category}
+        </h1>
+        <h1 className="text-lg break-words text-center font-sans font-medium tracking-wide">
+          {product_name}
+        </h1>
         <h1 className="text-center font-sans font-thin">Brand Name Here</h1>
       </div>
     </motion.div>
