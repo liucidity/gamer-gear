017cea0b7d6a9064c66328c49bf5242760846e57
Stanley Surjanto <stan.surjanto@gmail.com>
2022-12-20 20:40:20 -0700

    feat: render products on main card

--- src/components/BannerPlayerProductCard.tsx
+++ src/components/BannerPlayerProductCard.tsx
@@ -1,20 +1,31 @@
 import React from "react";
 import darkGeoBg from "../../public/dark_geometric_bg.jpg";
 import Image from "next/image"
+import BannerProductCard from "./Widgets/BannerProductCard";
 
 type Props = {
   photoURL: string;
+  playerGearArray: any[]
 }
 
-const BannerPlayerProductCard = ({photoURL}:Props) => {
+const BannerPlayerProductCard = ({photoURL, playerGearArray}:Props) => {
+  const bannerProductCards = playerGearArray.map((gear, index) => {
+    return (
+      <BannerProductCard
+        key={gear["id"]}
+        product_name={gear["product_name"]}
+        category={index}
+      />
+    );
+  });
   return (
     <div className="relative flex h-full w-full flex-col items-center rounded-md">
       <Image src={darkGeoBg} alt={"dark geometric background"} className="h-[10rem] w-full bg-center bg-no-repeat bg-contain rounded-t-md"></Image>
-      <div className="absolute mt-[5rem] h-40 w-40 rounded-full shadow-xl shadow-black/30 ring-4 ring-purple-main ring-offset-0 bg-white overflow-hidden">
+      <div className="absolute mt-[5rem] h-40 w-40 rounded-full shadow-xl shadow-black/30 ring-4 ring-purple-main ring-offset-0 bg-gray-main overflow-hidden">
         <Image src={photoURL} alt={"profile pic"} fill />
       </div>
       <div className="h-4/5 w-full bg-white rounded-b-md pb-2 px-2 pt-24">
-
+        {bannerProductCards}
       </div>
     </div>
   );
