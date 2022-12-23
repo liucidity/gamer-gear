1af72241c1bf3137cd65118369bec548cab5ab56
Stanley Surjanto <stan.surjanto@gmail.com>
2022-12-22 18:34:28 -0700

    prettier

--- src/components/Widgets/GameCard.tsx
+++ src/components/Widgets/GameCard.tsx
@@ -1,33 +1,49 @@
-import Image from 'next/image';
-import * as React from 'react';
-import { text } from 'stream/consumers';
+import Image from "next/image";
+import * as React from "react";
+import { text } from "stream/consumers";
 
 export interface Props {
-  gameImageSrc: string,
-  gameImageAlt: string,
-  gameLogoSrc: string,
-  gameLogoAlt: string,
-  imageStyle: string,
-  textStyle: string,
-  game: string,
-  setGame: any
-
+  gameImageSrc: string;
+  gameImageAlt: string;
+  gameLogoSrc: string;
+  gameLogoAlt: string;
+  imageStyle: string;
+  textStyle: string;
+  game: string;
+  setGame: any;
 }
 
-export function GameCard ({gameImageSrc,gameImageAlt,imageStyle, gameLogoAlt, gameLogoSrc,textStyle, game, setGame}: Props) {
-
-  
+export function GameCard({
+  gameImageSrc,
+  gameImageAlt,
+  imageStyle,
+  gameLogoAlt,
+  gameLogoSrc,
+  textStyle,
+  game,
+  setGame,
+}: Props) {
   return (
-    <div className='group flex flex-col items-center' onClick={()=> setGame({game})}>
-    <div className='gamecard'>
-      <Image src={gameImageSrc} alt={gameImageAlt} fill
-      className={imageStyle}/>
-      <div className='w-[8em] h-[2em] relative m-4'>
-
-        <Image src={gameLogoSrc} alt={gameLogoAlt} fill
-        className={textStyle}/>
+    <div
+      className="group flex flex-col items-center border-2 border-black"
+      onClick={() => setGame({ game })}
+    >
+      <div className="gamecard">
+        <Image
+          src={gameImageSrc}
+          alt={gameImageAlt}
+          fill
+          className={imageStyle}
+        />
+        <div className="relative m-4 h-[2em] w-[8em]">
+          <Image
+            src={gameLogoSrc}
+            alt={gameLogoAlt}
+            fill
+            className={textStyle}
+          />
+        </div>
       </div>
     </div>
-        </div>
   );
 }
