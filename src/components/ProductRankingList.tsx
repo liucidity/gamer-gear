7dce2140c660cba76e95edba24546c9074212591
Stanley Surjanto <stan.surjanto@gmail.com>
2022-12-27 17:05:23 -0700

    feat: build cards and import photos

--- src/components/ProductRankingList.tsx
+++ src/components/ProductRankingList.tsx
@@ -1,57 +1,52 @@
-import Image from 'next/image';
-import * as React from 'react';
-import { ProductRankingListItem } from './ProductRankingListItem';
-import 'chart.js/auto'
-import {Pie} from 'react-chartjs-2'
-import { Chart } from './Widgets/Chart';
+import * as React from "react";
+import { ProductRankingListItem } from "./ProductRankingListItem";
+import "chart.js/auto";
+import { Chart } from "./Widgets/Chart";
 
 type Props = {
-  items:any
-}
+  items: any;
+};
 
-export function ProductRankingList ({items}: Props) {
+export function ProductRankingList({ items }: Props) {
   // console.log(items)
 
   const chartData = {
     labels: items.map((item) => item.product_name),
     // datasets is an array of objects where each object represents a set of data to display corresponding to the labels above. for brevity, we'll keep it at one object
     datasets: [
-        {
-          label: 'Pro Choice',
-          data: items.map((item)=> item.player_peripherals.length),
-          // you can set indiviual colors for each bar
-          backgroundColor: [
-            '#C42BF0',
-            '#F02BBA',
-            '#F0622B',
-            '#57F02B',
-            '#2BB9F0',
-          ],
-          borderWidth: 2,
-        }
-    ]
-}
-
+      {
+        label: "Pro Choice",
+        data: items.map((item) => item.player_peripherals.length),
+        // you can set indiviual colors for each bar
+        backgroundColor: [
+          "#C42BF0",
+          "#F02BBA",
+          "#F0622B",
+          "#57F02B",
+          "#2BB9F0",
+        ],
+        borderWidth: 2,
+      },
+    ],
+  };
 
   return (
-    <div className='flex flex-col items-center'>
-      <div className='mb-8'>
-        {/* pie chart */}
-        <Chart chartData={chartData}/>
-        </div>
+    <div className="flex flex-col items-center m-2">
+      <div className="my-8">
+        <Chart chartData={chartData} />
+      </div>
 
       <ol>
-
-      {items.map((item)=>{
-        // console.log(product_name)
-        return(
-          <>
-          <li>
-            <ProductRankingListItem item={item}/>
-          </li>
-          </>
-        )
-      })}
+        {items.map((item) => {
+          // console.log(product_name)
+          return (
+            <>
+              <li>
+                <ProductRankingListItem item={item} />
+              </li>
+            </>
+          );
+        })}
       </ol>
     </div>
   );
--- src/components/ProductRankingListItem.tsx
+++ src/components/ProductRankingListItem.tsx
@@ -1,24 +1,53 @@
-import * as React from 'react';
+import * as React from "react";
+import { CldImage } from "next-cloudinary";
+import { defaults } from "chart.js";
 
 export interface Props {
-  item:any
+  item: any;
 }
 
-export function ProductRankingListItem ({item}: Props) {
+export function ProductRankingListItem({ item }: Props) {
+  const srcImgString = (item: any) => {
+    const productNameLower = item.product_name.toLowerCase();
+    const gProXhs = `gamer-gear/g pro x wireless headset se`;
+    const apexPro = `gamer-gear/apex pro tkl`;
+    const gProKeeb = `gamer-gear/g pro x mechanical keyboard`;
+    const gProKeebSE = `gamer-gear/g pro x mk se`;
+    switch (productNameLower) {
+      case "g pro x":
+        return gProXhs;
+      case "g pro x wireless headset":
+        return gProXhs;
+      case "g pro x headset":
+        return gProXhs;
+      case "g pro gaming headset":
+        return gProXhs;
+      case "apex pro":
+        return apexPro;
+      case "g pro mechanical keyboard":
+        return gProKeeb;
+      case "g pro x keyboard se":
+        return gProKeebSE;
+      default:
+        return `gamer-gear/${productNameLower}`;
+    }
+  };
   return (
-    <div className='flex flex-row items-center justify-between bg-blue p-2 m-0 h-12 w-[24rem]'>
-      <div className=''>
-        {/* image */}
+    <div className="mb-2 flex h-[4rem] w-[28rem] flex-row items-center justify-between rounded-md bg-slate-200 p-2 drop-shadow-sm">
+      <div className="relative flex aspect-square h-full items-center justify-center overflow-hidden rounded-md bg-slate-50 ">
+        <CldImage
+          src={srcImgString(item)}
+          alt={"product image"}
+          width={100}
+          height={100}
+        ></CldImage>
       </div>
-      <div>
-        {item.product_name}
-      </div>
-      <div className='flex flex-col items-center '>
-        <div>
-        <span className="inline-flex items-center justify-center px-2 py-1 text-xs font-bold leading-none text-red-100 bg-red-600 rounded-full">{item.player_peripherals.length}</span>
-        </div>
+      <div className="font-sans text-xl font-medium">{item.product_name}</div>
+      <div className="flex flex-col items-center ">
+        <span className="flex items-center justify-center rounded-full bg-gradient-to-br from-purple-light to-purple-dark px-2 py-1 text-xs font-bold text-off-white">
+          {item.player_peripherals.length}
+        </span>
       </div>
     </div>
-
   );
 }
